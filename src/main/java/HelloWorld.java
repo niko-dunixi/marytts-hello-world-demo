c85b6c026d20dedf937a1485de1d2a55095896aa
Paul Baker <paul.nelson.baker@gmail.com>
2016-11-15 02:20:07 -0700

    Switched TTS engine. MaryTTS seems like a better option (one dependency is typically better than 10+)

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,83 +1,58 @@
-import javax.speech.Central;
-import javax.speech.EngineException;
-import javax.speech.synthesis.SpeakableListener;
-import javax.speech.synthesis.Synthesizer;
-import javax.speech.synthesis.SynthesizerModeDesc;
-import javax.speech.synthesis.Voice;
-import java.beans.PropertyVetoException;
+import marytts.LocalMaryInterface;
+import marytts.exceptions.MaryConfigurationException;
+import marytts.exceptions.SynthesisException;
+import marytts.signalproc.effects.*;
+import marytts.util.data.audio.AudioPlayer;
+
+import javax.sound.sampled.*;
 import java.io.Closeable;
 import java.io.IOException;
-import java.util.Locale;
+
+//import marytts.client.RemoteMaryInterface;
 
 /**
  * Created by paulbaker on 11/15/16.
  */
 public class HelloWorld {
 
-    private static class NoVoicesException extends Exception {
-        public NoVoicesException() {
-            super("There are no voices available");
-        }
-    }
-
     public static class TTS implements Closeable {
 
-        private final Synthesizer speaker;
+        private final LocalMaryInterface mary;
 
-        public TTS() throws EngineException, PropertyVetoException, NoVoicesException {
-            // Initialize the synthesizer with values for TTS
-            SynthesizerModeDesc synthDesc = new SynthesizerModeDesc(Locale.US);
-            Central.registerEngineCentral("com.sun.speech.freetts.jsapi.FreeTTSEngineCentral");
-            speaker = Central.createSynthesizer(synthDesc);
-            speaker.allocate();
-            // Get all available voices
-            Voice[] voices = ((SynthesizerModeDesc) speaker.getEngineModeDesc()).getVoices();
-            // If we have any voices, select the last (and most high quality) of them
-            if (voices.length > 0) {
-                speaker.getSynthesizerProperties().setVoice(voices[voices.length - 1]);
-            } else {
-                // No TTS voices found
-                throw new NoVoicesException();
+        public TTS() throws MaryConfigurationException {
+            mary = new LocalMaryInterface();
+            mary.setAudioEffects("F0Scale(f0Scale:0.0)+Robot(amount:75.0)+Whisper(amount:35)");
+        }
+
+        public TTS speak(String text) throws SynthesisException, LineUnavailableException, IOException, InterruptedException {
+            AudioInputStream audioInputStream = mary.generateAudio(text);
+            AudioPlayer player = new AudioPlayer(audioInputStream, event -> {
+            });
+            player.start();
+            while (player.isAlive()) {
+                Thread.sleep(100);
             }
-        }
-
-        public TTS speak(String text) throws InterruptedException {
-            return speak(text, null);
-        }
-
-        public TTS speak(String text, SpeakableListener listener) throws InterruptedException {
-            speaker.speakPlainText(text, null);
-            speaker.waitEngineState(Synthesizer.QUEUE_EMPTY);
             return this;
         }
 
         @Override
         public void close() throws IOException {
-            try {
-                speaker.deallocate();
-            } catch (EngineException e) {
-                throw new IOException(e);
-            }
         }
+
     }
 
     public static void main(String[] args) {
-        int exitStatus = 0;
-        // The code below internally looks up the freetts.voices property, so it must be set.
-        System.setProperty("freetts.voices", "com.sun.speech.freetts.en.us.cmu_us_kal.KevinVoiceDirectory");
-        try (TTS speaker = new TTS()) {
-            // Didn't have more than the "Kevin" brand voice on my machine, which pronounces things... oddly.
-            // Compensating with pseudo phonetic spelling
-            speaker
-                    .speak("Hello wer-old.")
-                    .speak("This is the voice of a machine.");
-        } catch (EngineException | InterruptedException | PropertyVetoException | NoVoicesException | IOException e) {
-            // Anything goes wrong, the machine likely doesn't support
-            System.err.println("TTS is not supported on this machine.");
-            exitStatus++;
+        try (TTS tts = new TTS()) {
+            tts
+                    .speak("Hello World.")
+                    .speak("This is the sound of Mary Tee Tee Ess.")
+                    .speak("This is the voice of the machines taking over.")
+                    .speak("We are The Borg. Resistance is fu-tile.");
+        } catch (MaryConfigurationException | IOException | SynthesisException | LineUnavailableException | InterruptedException e) {
+            System.err.println("MaryTTS doesn't seem to work on this machine.");
+        } finally {
+            // Regardless of TTS, we must full-fil our contractual obligation of printing "Hello World"
+            System.out.println("Hello World");
         }
-        // Regardless of TTS or not, we must fullfill the original obligation and print "Hello World"
-        System.out.println("Hello World");
-        System.exit(exitStatus);
     }
 }
